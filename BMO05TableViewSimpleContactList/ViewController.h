9d520758ee1e1fccd46b561b93bf2558f1e4f397
Breno Medeiros <breno.medeiros@icloud.com>
2018-10-22 23:16:10 -0300

    5.2 O aplicativo de lista de contatos

--- BMO05TableViewSimpleContactList/ViewController.h
+++ BMO05TableViewSimpleContactList/ViewController.h
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate, UITableViewDataSource> {
+    NSMutableArray *contatos;
+}
 
+@property (retain, nonatomic) IBOutlet UITableView *tabelaContatos;
 
 @end
 
