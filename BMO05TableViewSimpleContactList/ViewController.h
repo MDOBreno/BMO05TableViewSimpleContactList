00a57846c0e1fbd6bd76b59bb23e615ec0ae47b3
Breno Medeiros <breno.medeiros@icloud.com>
2018-10-22 21:50:25 -0300

    Initial Commit

---
+++ BMO05TableViewSimpleContactList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMO05TableViewSimpleContactList
+//
+//  Created by Breno Medeiros on 22/10/18.
+//  Copyright © 2018 Breno Medeiros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BMO05TableViewSimpleContactList/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BMO05TableViewSimpleContactList
+//
+//  Created by Breno Medeiros on 22/10/18.
+//  Copyright © 2018 Breno Medeiros. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
